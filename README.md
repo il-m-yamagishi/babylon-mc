99a6bf39e6e4d3fe820afb873bffba8e6b1574df
m-yamagishi <m-yamagishi@infiniteloop.co.jp>
2024-07-07 17:06:58 +0900

    feat: Add UV VertexData

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # babylon-mc
 Minecraft-like game with Babylon.js
+
+## Installation
+
+### Requirements
+
+- [node.js 20.x](https://nodejs.org/)
+- [pnpm v9](https://pnpm.io/)
+- [Biome](https://biomejs.dev/) - for Linter/Formatter
+- Recommended: [vscode](https://code.visualstudio.com/) and [Biome extension](https://marketplace.visualstudio.com/items?itemName=biomejs.biome)
+
+### Run development server
+
+```s
+$ git clone https://github.com/il-m-yamagishi/babylon-mc.git
+$ cd babylon-mc
+$ pnpm install
+$ pnpm run dev
+$ open http://localhost:5173/babylon-mc/
+```
+
+### Build
+
+```s
+$ pnpm run build
+```
--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,16 @@ import { ArcRotateCamera } from "@babylonjs/core/Cameras/arcRotateCamera";
 import { Engine } from "@babylonjs/core/Engines/engine";
 import { DirectionalLight } from "@babylonjs/core/Lights/directionalLight";
 import "@babylonjs/core/Materials/standardMaterial";
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
+import { Texture } from "@babylonjs/core/Materials/Textures/texture";
+import { Color3 } from "@babylonjs/core/Maths/math.color";
 import { Vector3 } from "@babylonjs/core/Maths/math.vector";
+import { Mesh } from "@babylonjs/core/Meshes/mesh";
+import { VertexData } from "@babylonjs/core/Meshes/mesh.vertexData";
 import { MeshBuilder } from "@babylonjs/core/Meshes/meshBuilder";
 import { Scene } from "@babylonjs/core/scene";
 import { SkyMaterial } from "@babylonjs/materials/sky";
+import McTexture from "./assets/babylon-mc-texture.gif";
 
 async function main() {
   const canvas = document.getElementById(
@@ -49,18 +55,15 @@ async function main() {
   skyBox.infiniteDistance = true;
   skyBox.material = skyMaterial;
 
-  const baseBox = MeshBuilder.CreateBox("BaseBox", {}, scene);
-  baseBox.position.y = -0.5;
-
-  for (let x = -64; x < 64; x++) {
-    for (let z = -64; z < 64; z++) {
-      const b = baseBox.createInstance(`BaseBox_${x}_${z}`);
-      b.position.x = x;
-      b.position.z = z;
-    }
-  }
-
-  baseBox.setEnabled(false);
+  const mesh = new Mesh("Top", scene);
+  const vertexData = createFacetVertexData();
+  vertexData.applyToMesh(mesh);
+  const mat = new StandardMaterial("mat", scene);
+  mat.diffuseTexture = new Texture(McTexture, scene, {
+    samplingMode: Texture.NEAREST_SAMPLINGMODE,
+  });
+  mat.specularColor = new Color3(0, 0, 0);
+  mesh.material = mat;
 
   function render() {
     scene.render();
@@ -73,3 +76,19 @@ async function main() {
 }
 
 main();
+
+function createFacetVertexData(textureId = 2) {
+  const vertexData = new VertexData();
+  vertexData.positions = [0, 0, 0, 1, 0, 1, 0, 0, 1, 1, 0, 0];
+  vertexData.indices = [0, 1, 2, 0, 3, 1];
+  vertexData.normals = [0, 1, 0, 0, 1, 0, 0, 1, 0, 0, 1, 0];
+  const textureSize = 16;
+  const jitter = 1 / 2048;
+  const u0 = 0 + (textureId % textureSize) / textureSize + jitter;
+  const u1 = 0 + (1 + (textureId % textureSize)) / textureSize - jitter;
+  const v0 = 1 - Math.floor(textureId / textureSize) / textureSize - jitter;
+  const v1 = 1 - Math.floor(1 + textureId / textureSize) / textureSize + jitter;
+  vertexData.uvs = [u0, v0, u1, v1, u1, v0, u1, v0];
+
+  return vertexData;
+}
